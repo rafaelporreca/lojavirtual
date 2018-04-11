e2fbe93e9c8438c8201ad3d252ca5f04db8db8fb
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-11 14:22:30 -0300

    Criando operação de instanciação

--- src/main/java/br/com/rafaelporreca/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/LojaVirtualApplication.java
@@ -1,12 +1,32 @@
 package br.com.rafaelporreca.lojavirtual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rafaelporreca.lojavirtual.domain.Categoria;
+import br.com.rafaelporreca.lojavirtual.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaVirtualApplication {
+public class LojaVirtualApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
