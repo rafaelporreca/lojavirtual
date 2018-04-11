1eb9fb60b8887aeb8f3d3fc352cba2341efc5581
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-11 09:54:16 -0300

    Projeto criado

---
+++ src/main/java/br/com/rafaelporreca/LojaVirtualApplication.java
@@ -0,0 +1,12 @@
+package br.com.rafaelporreca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+}
