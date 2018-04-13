a8d7f3cc976ed15b539af32c1293465574c0de16
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-13 14:22:30 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/rafaelporreca/lojavirtual/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.rafaelporreca.lojavirtual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rafaelporreca.lojavirtual.domain.Cliente;
+import br.com.rafaelporreca.lojavirtual.repositories.ClienteRepository;
+import br.com.rafaelporreca.lojavirtual.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id){
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+
+}
