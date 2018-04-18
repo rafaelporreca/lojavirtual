cf297e9f995acf8055e08cb35e30c73c3fa1cc43
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-18 12:07:06 -0300

    Validação sintatica com Bean Validation

--- src/main/java/br/com/rafaelporreca/lojavirtual/services/CategoriaService.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.rafaelporreca.lojavirtual.domain.Categoria;
+import br.com.rafaelporreca.lojavirtual.dto.CategoriaDTO;
 import br.com.rafaelporreca.lojavirtual.repositories.CategoriaRepository;
 import br.com.rafaelporreca.lojavirtual.services.exceptions.DataIntegrityException;
 import br.com.rafaelporreca.lojavirtual.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 
 }
