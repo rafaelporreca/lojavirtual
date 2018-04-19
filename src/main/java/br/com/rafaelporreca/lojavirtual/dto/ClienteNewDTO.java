522e96fd132cf08edaf9f01ab9496a622f84566c
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-19 11:52:32 -0300

    Validacao customzada de CPF ou CNPJ na insercao de cliente

--- src/main/java/br/com/rafaelporreca/lojavirtual/dto/ClienteNewDTO.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/dto/ClienteNewDTO.java
@@ -2,6 +2,14 @@ package br.com.rafaelporreca.lojavirtual.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.rafaelporreca.lojavirtual.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 	/**
@@ -9,19 +17,28 @@ public class ClienteNewDTO implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	//dados cliente
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5,max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
 	private String email;
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
 	//dados endereço
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
 	//dados telefone
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
