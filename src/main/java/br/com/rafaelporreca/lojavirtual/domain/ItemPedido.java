be46baf988818fbae641ace40e9de8b643c484a4
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-24 14:59:10 -0300

    Inserindo Pedido

--- src/main/java/br/com/rafaelporreca/lojavirtual/domain/ItemPedido.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/domain/ItemPedido.java
@@ -76,11 +76,20 @@ public class ItemPedido implements Serializable{
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/br/com/rafaelporreca/lojavirtual/domain/PagamentoComCartao.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/domain/PagamentoComCartao.java
@@ -2,8 +2,11 @@ package br.com.rafaelporreca.lojavirtual.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import br.com.rafaelporreca.lojavirtual.domain.enums.EstadoPagamento;
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	
 	private static final long serialVersionUID = 1L;
