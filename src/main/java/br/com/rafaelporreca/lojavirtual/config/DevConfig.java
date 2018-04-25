ec2b1c7a30b45b294cf6f44317537df8bc20ca94
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-25 15:01:00 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/rafaelporreca/lojavirtual/config/DevConfig.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.rafaelporreca.lojavirtual.services.DBService;
+import br.com.rafaelporreca.lojavirtual.services.EmailService;
+import br.com.rafaelporreca.lojavirtual.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,6 +32,11 @@ public class DevConfig {
 		dbService.instantiateTestDataBase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
