ee013be6b4a3f3c6fe7ace5e50e18b9e607261ff
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-26 12:19:42 -0300

    Adicionando senha a Cliente

--- src/main/java/br/com/rafaelporreca/lojavirtual/dto/ClienteNewDTO.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/dto/ClienteNewDTO.java
@@ -26,6 +26,8 @@ public class ClienteNewDTO implements Serializable{
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
 	
 	//dados endereço
 	@NotEmpty(message="Preenchimento obrigatório")
@@ -152,6 +154,14 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
