d3578b0ebf08ba0098a9f7efb51519d0b74e8b7d
Rafael Porreca <rafael.porreca@gmail.com>
2018-04-30 10:12:43 -0300

    Restricao de conteudo: cliente so acessa seus pedidos

--- src/main/java/br/com/rafaelporreca/lojavirtual/services/PedidoService.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.rafaelporreca.lojavirtual.domain.Cliente;
 import br.com.rafaelporreca.lojavirtual.domain.ItemPedido;
 import br.com.rafaelporreca.lojavirtual.domain.PagamentoComBoleto;
 import br.com.rafaelporreca.lojavirtual.domain.Pedido;
@@ -14,11 +18,13 @@ import br.com.rafaelporreca.lojavirtual.domain.enums.EstadoPagamento;
 import br.com.rafaelporreca.lojavirtual.repositories.ItemPedidoRepository;
 import br.com.rafaelporreca.lojavirtual.repositories.PagamentoRepository;
 import br.com.rafaelporreca.lojavirtual.repositories.PedidoRepository;
+import br.com.rafaelporreca.lojavirtual.security.UserSS;
+import br.com.rafaelporreca.lojavirtual.services.exceptions.AuthorizationException;
 import br.com.rafaelporreca.lojavirtual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
 	@Autowired
@@ -29,20 +35,21 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
-	public Pedido find(Integer id){
-		
+
+	public Pedido find(Integer id) {
+
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
+
 	}
-	
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
@@ -50,25 +57,35 @@ public class PedidoService {
 		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto){
-			PagamentoComBoleto pagto = (PagamentoComBoleto)obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
-		for(ItemPedido ip : obj.getItens()) {
+
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
-			ip.setPedido(obj);						
+			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		
-		//emailService.sendOrderConfirmationHtmlEmail(obj);
+
+		// emailService.sendOrderConfirmationHtmlEmail(obj);
 		emailService.sendOrderConfirmationEmail(obj);
-		
+
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null)
+			throw new AuthorizationException("Acesso negado!");
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente  cliente = clienteService.find(user.getId());		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
