9b2e93b243c30640724d620c6cdb973624dad0ff
Rafael Porreca <rafael.porreca@gmail.com>
2018-05-04 12:05:13 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/rafaelporreca/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/LojaVirtualApplication.java
@@ -1,17 +1,11 @@
 package br.com.rafaelporreca.lojavirtual;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.rafaelporreca.lojavirtual.services.S3Service;
-
 @SpringBootApplication
 public class LojaVirtualApplication implements CommandLineRunner{
-	
-	@Autowired
-	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -20,8 +14,6 @@ public class LojaVirtualApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		s3Service.uploadFile("C:\\temp\\havaianas-de-pau.jpg");
 		
 	}
 }
