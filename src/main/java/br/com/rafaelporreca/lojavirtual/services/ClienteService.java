fc1b4a4b4e0c03529eae114970a79f84e575353c
Rafael Porreca <rafael.porreca@gmail.com>
2018-05-04 15:04:49 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/br/com/rafaelporreca/lojavirtual/services/ClienteService.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/services/ClienteService.java
@@ -115,8 +115,16 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
 		
-		return s3Service.uploadFile(multipartFile);
+		URI uri = s3Service.uploadFile(multipartFile);
+		Cliente cli = find(user.getId());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		return uri;
 		
 	}
 
