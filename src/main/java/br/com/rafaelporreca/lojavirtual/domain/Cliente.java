7605f74148ad43d2a7a46794f44420fbf75561ce
Rafael Porreca <rafael.porreca@gmail.com>
2018-05-07 10:23:06 -0300

    Ajustando tamanho da imagem

--- src/main/java/br/com/rafaelporreca/lojavirtual/domain/Cliente.java
+++ src/main/java/br/com/rafaelporreca/lojavirtual/domain/Cliente.java
@@ -54,7 +54,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -179,17 +178,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-
-	
-	
 
 }
